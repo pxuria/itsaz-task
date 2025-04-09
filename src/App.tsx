b613a230008634b67ac72edc81b01df7e3973cec
pxuria <pouriamesram@gmail.com>
2025-04-09 14:43:58 +0330

    fix configurations

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
+import { Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+
 function App() {
   return (
-    <>
-      <h1 className="font-bold">hello</h1>
-      <h1 className="font-light">hello</h1>
-    </>
+    <Routes>
+      <Route path="/" element={<Home />} />
+    </Routes>
   );
 }
 
