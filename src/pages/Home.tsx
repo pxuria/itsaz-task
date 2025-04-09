27e622fbc8910347bb5cf44ffff2dde19aa2ca8c
pxuria <pouriamesram@gmail.com>
2025-04-09 15:32:53 +0330

    add sidebar

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,17 @@
+import SidebarLayout from "@/components/shared/SidebarLayout";
+import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";
+
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div className="">
+      <SidebarProvider>
+        <SidebarLayout />
+        <SidebarTrigger />
+
+        <div className="bg-[#fff] h-[calc(90vh)] w-10/12 rounded-2xl mt-[5vh] mx-auto"></div>
+      </SidebarProvider>
+    </div>
+  );
 };
 
 export default Home;
